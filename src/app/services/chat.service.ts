9ae0dfc2ecb45c1b393fa67ecfb6577acc14f91b
marcosjrs <marcosjrs80@gmail.com>
2018-11-07 12:01:45 +0100

    Recogiendo solo los ultimos 5 mensajes de forma desc, y luego un revese

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Mensaje } from 'src/app/models/mensaje';
 // https://github.com/angular/angularfire2/blob/master/docs/firestore/collections.md
@@ -13,11 +14,15 @@ export class ChatService {
   mensajes: Observable<Mensaje[]>;
 
   constructor(db: AngularFirestore) {
-    this.mensajesCollection = db.collection<Mensaje>('chats');
-    this.mensajes = this.mensajesCollection.valueChanges();
+    this.mensajesCollection = db.collection<Mensaje>( 'chats', 
+                                                 ref => ref.orderBy('fecha', 'desc').limit(5) );
+    this.mensajes = this.mensajesCollection.valueChanges().pipe(
+      map( msgs => msgs.reverse())
+    );
   }
 
   enviarMsg(msg:string) {
+    if(!msg) return;
     const nombreUser = "Marcos";
     const uidUser = "123";
     let mensaje:Mensaje = {"nombre":nombreUser, "mensaje":msg, "fecha":new Date().getTime()}; //con new Mensaje revienta ...
